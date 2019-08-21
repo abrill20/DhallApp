9d89e4562298bf8dfdb306280334722653cb41f1
Aaron Brill <abrill@skidmore.edu>
2019-08-21 16:19:01 -0400

    Add search feature

--- DhallApp/MenuTableViewController.swift
+++ DhallApp/MenuTableViewController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 
-class MenuTableViewController: UITableViewController {
+class MenuTableViewController: UITableViewController, UISearchResultsUpdating {
+    
     
     var foods = [Food]()
+    var filteredTableFoods = [Food]()
+    var resultSearchController = UISearchController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        resultSearchController = ({
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.sizeToFit()
+            
+            tableView.tableHeaderView = controller.searchBar
+            
+            return controller
+        })()
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredTableFoods.removeAll(keepingCapacity: false)
+        
+        guard let text = searchController.searchBar.text else {return}
+        filteredTableFoods = foods.filter { (food: Food) in
+            return food.name.prefix(text.count).lowercased() == text.lowercased()
+        }
+        
+        self.tableView.reloadData()
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -20,6 +45,11 @@ class MenuTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (resultSearchController.isActive) {
+            return filteredTableFoods.filter { (food: Food) in
+                return food.station?.rawValue == section
+            }.count
+        }
         let correctStation = foods.filter { (food: Food) in
             return food.station?.rawValue == section      }
         return correctStation.count
@@ -28,6 +58,14 @@ class MenuTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Food", for: indexPath)
+        
+        if(resultSearchController.isActive) {
+            let correctStation = filteredTableFoods.filter { (food: Food) in
+                return food.station?.rawValue == indexPath.section
+            }
+            cell.textLabel?.text = correctStation[indexPath.row].name
+            return cell
+        }
         
         let correctStation = foods.filter { (food: Food) in
             return food.station?.rawValue == indexPath.section
